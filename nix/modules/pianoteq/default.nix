4e7a4a8f866e6b3e607d958fb1902da83aa007c5
Joost Nieuwenhuijse <joost@newhouse.nl>
2026-01-22 10:09:40 +0100

    -

---
+++ nix/modules/pianoteq/default.nix
@@ -0,0 +1,74 @@
+#https://github.com/qhga/nix-pianoteq7/blob/main/flake.nix
+{ pkgs, ... }:
+with pkgs;
+stdenv.mkDerivation rec {
+  pname = "pianoteq";
+  version = "9.0.3";
+
+  icon = fetchurl {
+    name = "pianoteq_icon_128";
+    url = "https://www.pianoteq.com/images/logo/pianoteq_icon_128.png";
+    sha256 = "sha256-lO5kz2aIpJ108L9w2BHnRmq6wQP+6rF0lqifgor8xtM=";
+  };
+
+  src = ./pianoteq_setup_v903.tar.xz;
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "pianoteq9";
+      desktopName = "Pianoteq 9";
+      exec = "pianoteq9";
+      icon = "pianoteq_icon_128";
+    })
+  ];
+
+  nativeBuildInputs = [
+    xz
+    gnutar
+    copyDesktopItems
+  ];
+
+  libPath = lib.makeLibraryPath [
+    alsa-lib
+    freetype
+    xorg.libX11
+    xorg.libXext
+    stdenv.cc.cc.lib
+    libjack2
+    lv2
+    libGL
+    fontconfig
+  ];
+
+  unpackCmd = "tar -xJf ${src}";
+
+  # `runHook postInstall` is mandatory otherwise postInstall won't run
+  installPhase = ''
+    install -Dm 755 x86-64bit/Pianoteq\ 9 $out/bin/pianoteq9
+    install -Dm 755 x86-64bit/Pianoteq\ 9.lv2/Pianoteq_9.so \
+                    $out/lib/lv2/Pianoteq\ 9.lv2/Pianoteq_9.so
+    patchelf --set-interpreter "$(< $NIX_CC/nix-support/dynamic-linker)" \
+              --set-rpath $libPath "$out/bin/pianoteq9"
+    cd x86-64bit/Pianoteq\ 9.lv2/
+    for i in *.ttl; do
+        install -D "$i" "$out/lib/lv2/Pianoteq 9.lv2/$i"
+    done
+    runHook postInstall
+  '';
+
+  # This also works instead of the following
+  # makeWrapper $out/bin/pianoteq7 $out/bin/pianoteq7_wrapped --prefix LD_LIBRARY_PATH : "$libPath"
+  fixupPhase = '':'';
+
+  # Runs copyDesktopItems hook.
+  # Alternatively call copyDesktopItems manually in installPhase/fixupPhase
+  postInstall = ''
+    install -Dm 444 ${icon} $out/share/icons/hicolor/128x128/apps/pianoteq_icon_128.png
+  '';
+
+  meta = {
+    homepage = "https://www.modartt.com/";
+    description = "Pianoteq is a virtual instrument which in contrast to other virtual instruments is physically modelled and thus can simulate the playability and complex behaviour of real acoustic instruments. Because there are no samples, the file size is just a tiny fraction of that required by other virtual instruments.";
+    platforms = lib.platforms.linux;
+  };
+}
